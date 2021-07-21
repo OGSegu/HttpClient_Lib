cf82f7295298bd56b94fd0a1159e44d4d8821c13
OGSegu <krivitskij.vv@edu.spbstu.ru>
2021-07-21 18:52:58 +0300

    Added logger, extract some code into method

--- src/utils/DefaultHttpHandler.java
+++ src/utils/DefaultHttpHandler.java
@@ -8,46 +8,59 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DefaultHttpHandler extends HttpHandler  {
+
+    private static final Logger logger = Logger.getAnonymousLogger();
 
     @Override
     public void completed(AsynchronousSocketChannel socketChannel, AsynchronousServerSocketChannel serverSocketChannel) {
         try {
-            System.out.println("Received connection");
+            logger.log(Level.INFO, "Received connection from: {0}", socketChannel.getRemoteAddress());
             serverSocketChannel.accept(serverSocketChannel, this);
-            StringBuilder sb = new StringBuilder();
-            ByteBuffer byteBuffer = ByteBuffer.allocate(BYTE_LIMIT);
-            boolean keepReading = true;
-            while (keepReading) {
-                socketChannel.read(byteBuffer).get();
-
-                keepReading = byteBuffer.position() == BYTE_LIMIT;
-
-                byte[] arrayHttpResponse = keepReading
-                        ? byteBuffer.array()
-                        : Arrays.copyOfRange(byteBuffer.array(), 0, byteBuffer.position());
-
-                sb.append(new String(arrayHttpResponse));
-                byteBuffer.clear();
-            }
-            System.out.println(sb);
-            socketChannel.write(ByteBuffer.wrap("Your input was received. Thank you\r\n".getBytes()));
+            StringBuilder sb = readRequest(socketChannel);
+            logger.log(Level.INFO, "Received data:\n{0}", sb);
+            socketChannel.write(ByteBuffer.wrap("Your input was received. Thank you\n".getBytes()));
         } catch (ExecutionException e) {
-            e.printStackTrace();
+            logger.log(Level.WARNING,"Failed to read request:\n{0}", e.getCause().getMessage());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
+        } catch (IOException e) {
+            logger.warning("Failed to get remote socket address");
         } finally {
             try {
                 socketChannel.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.warning("Failed to close connection");
             }
         }
+    }
+
+    private StringBuilder readRequest(AsynchronousSocketChannel socketChannel) throws InterruptedException, ExecutionException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(BYTE_LIMIT);
+        StringBuilder sb = new StringBuilder();
+        boolean keepReading = true;
+        while (keepReading) {
+            socketChannel.read(byteBuffer).get();
+
+            keepReading = byteBuffer.position() == BYTE_LIMIT;
+
+            byte[] arrayHttpResponse = keepReading
+                    ? byteBuffer.array()
+                    : Arrays.copyOfRange(byteBuffer.array(), 0, byteBuffer.position());
+
+            sb.append(new String(arrayHttpResponse));
+            byteBuffer.clear();
+        }
+        return sb;
     }
 
     @Override
     public void failed(Throwable exc, AsynchronousServerSocketChannel attachment) {
         exc.printStackTrace();
     }
+
+
 }
